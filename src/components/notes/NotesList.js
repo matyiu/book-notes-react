807359f5393cdc2490cb6c30750b19e156c0c76e
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-03-08 15:22:07 +0100

    Fixed note list view issues

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -2,28 +2,36 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Alert, Button, Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { selectAllAuthors, selectAllCategories } from '../../redux/tagsSlice';
 
 export const NotesList = () => {
     const notes = useSelector(status => status.notes);
+    const authors = useSelector(selectAllAuthors);
+    const categories = useSelector(selectAllCategories);
 
     const renderedNotes = notes.map(
-        note => (
-            <Col xs="12" key={note.id}>
-                <Link to={`/book/${note.id}`}>
-                    <article className="note">
-                        <h2>{note.name}</h2>
-                        <div className="note-metadata">
-                            <div className="tag">{note.state}</div>
-                            <div className="tag">{note.category}</div>
-                            <div className="tag">{note.author}</div>
-                        </div>
-                        <Button className="note-action" variant="link">
-                            <i className="fas fa-trash"></i>
-                        </Button>
-                    </article>
-                </Link>
-            </Col>
-        )
+        note => {
+            const author = authors.find(author => author.value === note.author);
+            const category = categories.find(category => category.value === note.category);
+
+            return (
+                <Col xs="12" key={note.id}>
+                    <Link to={`/book/${note.id}`}>
+                        <article className="note">
+                            <h2>{note.name}</h2>
+                            <div className="note-metadata">
+                                <div className="tag">{note.state}</div>
+                                <div className="tag">{category.content}</div>
+                                <div className="tag">{author.content}</div>
+                            </div>
+                            <Button className="note-action" variant="link">
+                                <i className="fas fa-trash"></i>
+                            </Button>
+                        </article>
+                    </Link>
+                </Col>
+            );
+        }
     );
 
     return (
