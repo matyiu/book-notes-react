8cf4b18053d3bfe4a44bb684e1516187f775724b
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-03-11 14:05:04 +0100

    Added consistent styles with the app to the exported html

--- src/components/buttons/ExportToHTML.js
+++ src/components/buttons/ExportToHTML.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { saveAs } from 'file-saver';
+import { exportHtmlStyles } from '../../variables/exportHtmlStyles';
 
 export const ExportToHTML = ({ notes, name }) => {
+    const headTag = `
+        <head>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+            <style>
+                ${exportHtmlStyles}
+            </style>
+        </head>
+    `;
+    const bodyTag = `
+        <body>
+            <div class="container">
+                ${notes}
+            </div>
+        </body>
+    `
+
     // Export Handlers
     const exportToHtml = () => {
-        const blob = new Blob([notes], {type: 'text/html'});
+        const blob = new Blob([headTag, bodyTag], {type: 'text/html'});
         const formatedName = name.replace(' ', '_') + '.html';
         
         saveAs(blob, formatedName);
---
+++ src/variables/exportHtmlStyles.js
@@ -0,0 +1,34 @@
+export const exportHtmlStyles = `
+    body {
+        background: #fff7f3;
+        font-family: 'Roboto Condensed', sans-serif;
+        color: #445554;
+        font-size: 1.25rem;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        color: #223A38;
+        margin-top: 30px;
+        margin-bottom: 15px;
+    }
+
+    p {
+        margin-top: 0;
+        margin-bottom: 20px;
+    }
+
+    a {
+        color: #445554;
+        text-decoration: underline;
+    }
+
+    a:hover {
+        color: #223A38;
+    }
+
+    body > .container {
+        max-width: 700px;
+        margin: 0 auto;
+        padding: 0 15px;
+    }
+`;
