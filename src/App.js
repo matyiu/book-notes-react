fe8c02af7984f93afb1a55524de5b8a82015b328
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-09-23 14:04:11 +0200

    Created global styles and added body styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Book } from './pages/Book';
+import { createGlobalStyle } from 'styled-components';
+import { darkTheme } from './variables/colors';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <GlobalStyle />
         <Switch>
           <Route exact path="/book/:bookId">
             <Book />
@@ -19,5 +22,12 @@ function App() {
     </BrowserRouter>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${darkTheme.background};
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+`;
 
 export default App;
