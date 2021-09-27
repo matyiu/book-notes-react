04434e66692aa365968ad386a47c1ab7231fd88c
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-09-27 13:42:43 +0200

    Added Sidebar to Book page

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Navbar } from '../components/Navbar';
+import { Col, Container, Row } from '../components/grid/grid';
 import { SingleNote } from '../components/notes/SingleNote';
+import { Sidebar } from '../components/Sidebar';
 
 export const Book = () => {
     return (
         <div id="bookpage">
             <Navbar />
-            {/* <SingleNote /> */}
+            <Container>
+                <Row>
+                    <Col colNumber={3}>
+                        <Sidebar />
+                    </Col>
+                    <Col colNumber={9}>
+                        <SingleNote />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
