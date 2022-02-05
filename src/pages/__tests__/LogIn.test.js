d27bb4f1a0c665b091d6eac0b0fe150dc91c3cd4
Jefferson Valle <valleespinozajefferson@gmail.com>
2022-02-05 10:07:03 +0100

    Test error messages disappearance when input is filled

--- src/pages/__tests__/LogIn.test.js
+++ src/pages/__tests__/LogIn.test.js
@@ -3,6 +3,7 @@ import { LogIn } from "../LogIn";
 import { fireEvent, render, screen } from "../../test-utils";
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../../redux/authSlice";
+import { waitForElementToBeRemoved } from "@testing-library/react";
 
 const renderAuthStore = () => {
   return render(<LogIn />, {
@@ -50,4 +51,36 @@ it("Form doesn't submit with blank input values", async () => {
   fireEvent.click(submitButton, { name: "Log In" });
   expect(await screen.findByText("Username or email is required")).toBeTruthy();
   expect(await screen.findByText("Password is empty")).toBeTruthy();
+});
+
+it("Username errors disappear when input is filled", async () => {
+  renderAuthStore();
+
+  const { usernameField, submitButton } = getLogInFields();
+
+  fireEvent.click(submitButton, { name: "Log In" });
+  expect(await screen.findByText("Username or email is required")).toBeTruthy();
+
+  changeFieldValue(usernameField, "user");
+  fireEvent.click(submitButton, { name: "Log In" });
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("Username or email is required")
+  );
+  expect(screen.queryByText("Username or email is required")).toBeNull();
+});
+
+it("Username errors disappear when input is filled", async () => {
+  renderAuthStore();
+
+  const { passwordField, submitButton } = getLogInFields();
+
+  fireEvent.click(submitButton, { name: "Log In" });
+  expect(await screen.findByText("Password is empty")).toBeTruthy();
+
+  changeFieldValue(passwordField, "newpassword");
+  fireEvent.click(submitButton, { name: "Log In" });
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("Password is empty")
+  );
+  expect(screen.queryByText("Password is empty")).toBeNull();
 });
