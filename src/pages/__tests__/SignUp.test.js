4ee9490a5fc4b110d2ee248944cd05faa8e7b337
Jefferson Valle <valleespinozajefferson@gmail.com>
2022-02-07 14:55:23 +0100

    Test front end validation errors

--- src/pages/__tests__/SignUp.test.js
+++ src/pages/__tests__/SignUp.test.js
@@ -3,7 +3,6 @@ import { SignUp } from "../SignUp";
 import { changeFieldValue, fireEvent, render, screen } from "../../test-utils";
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../../redux/authSlice";
-import { waitForElementToBeRemoved } from "@testing-library/react";
 
 const renderAuthStore = () => {
   return render(<SignUp />, {
@@ -29,29 +28,87 @@ const getFields = () => {
   };
 };
 
+const testFieldFrontValidation = async (field, value, msg) => {
+  const { submitButton } = getFields();
+  changeFieldValue(field, value);
+  fireEvent.click(submitButton, { name: "Sign Up" });
+  expect(await screen.findByText(msg)).toBeTruthy();
+};
+
 describe("Front End Validation", () => {
-  it("Displays username error when the field is empty", async () => {
+  beforeEach(() => {
     renderAuthStore();
+  });
 
-    const { usernameField, submitButton } = getFields();
-    fireEvent.click(submitButton, { name: "Sign Up" });
-    expect(await screen.findByText("Username is required")).toBeTruthy();
+  it("Displays username error when the field is empty", async () => {
+    const { usernameField } = getFields();
 
-    changeFieldValue(usernameField, "         ");
-    fireEvent.click(submitButton, { name: "Sign Up" });
-    expect(await screen.findByText("Username is required")).toBeTruthy();
+    await testFieldFrontValidation(usernameField, "", "Username is required");
+
+    await testFieldFrontValidation(
+      usernameField,
+      "    ",
+      "Username is required"
+    );
   });
 
   it("Displays username error when the field has whitespaces", async () => {
-    renderAuthStore();
+    const { usernameField } = getFields();
 
-    const { usernameField, submitButton } = getFields();
-    changeFieldValue(usernameField, "user with spaces");
-    fireEvent.click(submitButton, { name: "Sign Up" });
-    expect(
-      await screen.findByText(
-        "Whitespaces are not allowed on the username field"
-      )
-    ).toBeTruthy();
+    await testFieldFrontValidation(
+      usernameField,
+      "user with spaces",
+      "Whitespaces are not allowed on the username field"
+    );
+  });
+
+  it("Displays name errors when field is empty", async () => {
+    const { nameField } = getFields();
+
+    await testFieldFrontValidation(nameField, "", "Name is required");
+
+    await testFieldFrontValidation(nameField, "    ", "Name is required");
+  });
+
+  it("Displays email errors when field is empty", async () => {
+    const { emailField } = getFields();
+
+    await testFieldFrontValidation(emailField, "", "Email is required");
+
+    await testFieldFrontValidation(emailField, "    ", "Email is required");
+  });
+
+  it("Displays password errors when field is empty", async () => {
+    const { passwordField } = getFields();
+
+    await testFieldFrontValidation(passwordField, "", "Password is required");
+
+    await testFieldFrontValidation(
+      passwordField,
+      "    ",
+      "Password is required"
+    );
+  });
+
+  it("Displays password errors when value is less than 10 characters", async () => {
+    const { passwordField } = getFields();
+
+    await testFieldFrontValidation(
+      passwordField,
+      "123",
+      "The password must be greater than 10"
+    );
+  });
+
+  it("Displays password errors when confirm password and password fields aren't equal", async () => {
+    const { passwordField, confirmPasswordField } = getFields();
+
+    changeFieldValue(passwordField, "1234567890");
+
+    await testFieldFrontValidation(
+      confirmPasswordField,
+      "1234567890 not equal",
+      "The password and the confirmation password do not match"
+    );
   });
 });
