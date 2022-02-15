50e2f3e88a841b53b00e11e835ddf4ffd0a352a7
Jefferson Valle <valleespinozajefferson@gmail.com>
2022-02-15 17:16:21 +0100

    Removed local storage save on redux state change

--- src/index.js
+++ src/index.js
@@ -6,15 +6,9 @@ import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
-import { saveData } from './app/localStorage'
 import initData from './app/initData'
 import { setNotes } from './redux/notesSlice'
 import { setTags } from './redux/tagsSlice'
-
-store.subscribe(() => {
-    saveData('boonote.notes', store.getState().notes)
-    saveData('boonote.tags', store.getState().tags)
-})
 
 initData().then((res) => {
     if (res) {
